bc38afb1e41f2c2505bdc1753a432c70d107e20e
Jackson McComas <jackson.mccomas@gmail.com>
2020-04-14 23:52:35 -0500

    making progress on  the ancestor graph

--- islands.py
+++ islands.py
@@ -15,5 +15,58 @@
 # How can we find the extent of an island
 # Either of our travels to find all of the nodes in an island
 
-# How do I explore the larder set?
+# How do I explore the larger set?
 # Loop thorough and call a traversal if we find an alternative
+
+islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+
+
+def island_counter(islands):
+    num_islands = 0
+    # iterate through the islands
+    for y in range(0, len(islands)):
+        for x in range(0, len(islands[y])):
+            # if the island-node is part of an island and hasn't been visited
+            if islands[y][x] == 1:
+                # you've found an island, so increment
+                num_islands += 1
+                # BFT through the island-node's neighbors
+                islands = visit_neighbors(x, y, islands)
+    return num_islands
+
+
+def visit_neighbors(x, y, islands):
+    # NORTH
+    if y > 0:  # if not at northern edge
+        # and neighbor is an island-node
+        if islands[y-1][x] == 1:
+            # mark it visited by updating value to 0
+            islands[y-1][x] = 0
+            # and visit all it's neighbors
+            islands = visit_neighbors(x, y-1, islands)
+
+    # SOUTH
+    if y < 4:
+        if islands[y+1][x] == 1:
+            islands[y+1][x] = 0
+            islands = visit_neighbors(x, y+1, islands)
+    # EAST
+    if x < 4:
+        if islands[y][x+1] == 1:
+            islands[y][x+1] = 0
+            islands = visit_neighbors(x+1, y, islands)
+    # WEST
+    if x > 0:
+        if islands[y][x-1] == 1:
+            islands[y][x-1] = 0
+            islands = visit_neighbors(x-1, y, islands)
+    return islands
+
+
+result = island_counter(islands)
+
+print(result)
--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,18 @@
+# Task: Search the given ancestor graph to the earliest one.
+
+from graph import Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # create a graph out of ancestors
+    graph = Graph()
+    for x in range(1, 12):
+        graph.add_vertex(x)
+    for x in ancestors:
+        # graph.add_edge(x[0], x[1])
+        graph.add_edge(x[1], x[0])
+    return graph.vertices
+
+
+print(earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6),
+                         (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)], 1))
